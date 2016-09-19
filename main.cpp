093fa8c5d3b3789f2c965196ffc233661e900f15
danielloza11 <danielloza11@wp.pl>
2016-09-19 21:51:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,66 @@
 
 using namespace std;
 
+int i;
+string pole [70][70];
+
+
+void stworz_pole()
+     {
+
+    for( i=0;i<=70;i++)
+        {
+     int a=-1;
+     int b=0;
+
+     do
+        {
+           a=a+1;
+           pole[a][b]="0";
+
+        }while(a<69);
+        b=b+1;
+
+}
+
+    }
+
+
+    void rysuj_pole()
+     {
+
+    for( i=0;i<=70;i++)
+        {
+     int a=-1;
+     int b=0;
+
+     do
+        {
+           a=a+1;
+           cout<<pole[a][b]<<" ";
+
+        }while(a<69);
+        b=b+1;
+        cout<<endl;
+
+}
+
+    }
+
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
-} Daniel to ciota
 
+
+stworz_pole();
+
+rysuj_pole();
+
+
+
+
+
+
+    return 0;
+}
